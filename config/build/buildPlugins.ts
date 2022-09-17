8155791c708373d2b9fdb805eaea89dcdb608071
dfkadyr <dfkadyr@gmail.com>
2022-09-17 22:13:56 +0300

    add config build folder

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins(props: BuildOptions): webpack.WebpackPluginInstance[] {
+  const { paths } = props;
+
+  return [
+    new HTMLWebpackPlugin({
+      template: paths.html
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    })
+  ]
+}
