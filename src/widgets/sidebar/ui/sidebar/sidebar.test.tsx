c41986f1d8b275984e6c3326db8e82adb91af223
dfkadyr <dfkadyr@gmail.com>
2022-10-05 19:14:42 +0300

    upgrade sidebar, add button themes

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react'
 
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation'
+import { componentRender } from 'shared/lib/tests/component-render'
 import { Sidebar } from 'widgets/sidebar'
 
 describe('Sidebar', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     fireEvent.click(toggleBtn)
