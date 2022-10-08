cfcb4c4a616e4c0b53642afbe2f4dee6ab167b7d
dfkadyr <dfkadyr@gmail.com>
2022-10-08 23:19:42 +0300

    add json-server

---
+++ json-server/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const path = require('path')
+
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
+
+server.use(jsonServer.defaults({}))
+server.use(jsonServer.bodyParser)
+
+// Imitation of a real api
+server.use(async (req, res, next) => {
+  // eslint-disable-next-line promise/param-names
+  await new Promise((res) => {
+    setTimeout(res, 800)
+  })
+  next()
+})
+
+// Endpoint for login
+server.post('/login', (req, res) => {
+  try {
+    const { username, password } = req.body
+    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'))
+    const { users = [] } = db
+
+    const userFromBd = users.find(
+      (user) => user.username === username && user.password === password
+    )
+
+    if (userFromBd) {
+      return res.json(userFromBd)
+    }
+
+    return res.status(403).json({ message: 'User not found' })
+  } catch (e) {
+    console.error(`POST-login ${e}`)
+    return res.status(500).json({ message: e.message })
+  }
+})
+
+// check if the user is logged in
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: 'AUTH ERROR' })
+  }
+
+  next()
+})
+
+server.use(router)
+
+// server start
+server.listen(8000, () => {
+  console.log('server is running on 8000 port')
+})
