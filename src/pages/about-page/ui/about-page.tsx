8ecda1aa47253042decf72393e45a52dacd29caa
dfkadyr <dfkadyr@gmail.com>
2022-10-08 19:53:29 +0300

    remove counter from page

--- src/pages/about-page/ui/about-page.tsx
+++ src/pages/about-page/ui/about-page.tsx
@@ -1,14 +1,11 @@
 import { useTranslation } from 'react-i18next'
 
-import { Counter } from 'entities/counter'
-
 export const AboutPage = (): JSX.Element => {
   const { t } = useTranslation('about')
 
   return (
     <div>
       {t('About page')}
-      <Counter />
     </div>
   )
 }
