b8fd896d7be0778a53fc7fa607fdc4d656f77e1c
dfkadyr <dfkadyr@gmail.com>
2022-10-17 09:33:36 +0300

    fixed issue

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -37,7 +37,11 @@ export default {
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
-  }
+  },
+  transformIgnorePatterns: [
+    '\\\\node_modules\\\\',
+    '\\.pnp\\.[^\\\\]+$'
+  ]
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
@@ -172,12 +176,6 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
