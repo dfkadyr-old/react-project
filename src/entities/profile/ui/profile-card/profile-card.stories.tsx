f7faa34759ce0cdd9334f734984c8bf8aaf69845
dfkadyr <dfkadyr@gmail.com>
2022-10-28 08:52:20 +0300

    fixed storybook

--- src/entities/profile/ui/profile-card/profile-card.stories.tsx
+++ src/entities/profile/ui/profile-card/profile-card.stories.tsx
@@ -13,38 +13,28 @@ export default {
   component: ProfileCard,
   argTypes: {
     backgroundColor: { control: 'color' }
+  },
+  args: {
+    data: {
+      username: 'admin',
+      age: 22,
+      country: Country.Ukraine,
+      lastname: 'dfkadyr',
+      first: 'asd',
+      city: 'asf',
+      currency: Currency.USD,
+      avatar: AvatarImg
+    }
   }
 } as ComponentMeta<typeof ProfileCard>
 
 const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />
 
 export const Primary = Template.bind({})
-Primary.args = {
-  data: {
-    username: 'admin',
-    age: 22,
-    country: Country.Ukraine,
-    lastname: 'dfkadyr',
-    first: 'asd',
-    city: 'asf',
-    currency: Currency.USD,
-    avatar: AvatarImg
-  }
-}
+Primary.args = {}
 
 export const PrimaryDark = Template.bind({})
-PrimaryDark.args = {
-  data: {
-    username: 'admin',
-    age: 22,
-    country: Country.Ukraine,
-    lastname: 'dfkadyr',
-    first: 'asd',
-    city: 'asf',
-    currency: Currency.USD,
-    avatar: AvatarImg
-  }
-}
+PrimaryDark.args = {}
 PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)]
 
 export const isLoading = Template.bind({})
@@ -59,15 +49,5 @@ withError.args = {
 
 export const isReadonly = Template.bind({})
 isReadonly.args = {
-  data: {
-    username: 'admin',
-    age: 22,
-    country: Country.Ukraine,
-    lastname: 'dfkadyr',
-    first: 'asd',
-    city: 'asf',
-    currency: Currency.USD,
-    avatar: AvatarImg
-  },
   isReadonly: true
 }
