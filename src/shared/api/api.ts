29dd37fb97f4592d1cc7fbcb15d0403646b491fc
dfkadyr <dfkadyr@gmail.com>
2022-11-23 00:45:58 +0300

    replace api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,11 +2,10 @@ import axios from 'axios'
 
 import { AUTH_USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage'
 
-export const $api = axios.create({
-  baseURL: process.env.API_URL
-})
+export const $api = axios.create({})
 
 $api.interceptors.request.use((config) => {
+  config.baseURL = process.env.API_URL
   if (config.headers) {
     config.headers.Authorization = localStorage.getItem(AUTH_USER_LOCALSTORAGE_KEY) ?? ''
   }
