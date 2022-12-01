e79d03bd6d6f0a3d012788a59ff4d00bbb833e99
dfkadyr <dfkadyr@gmail.com>
2022-12-01 07:58:53 +0300

    add jest-html-reporters

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -40,7 +40,16 @@ export default {
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
-  }
+  },
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: '<rootDir>/reports/unit',
+      filename: 'report.html',
+      openReport: true,
+      inlineSource: true
+    }]
+  ]
   // transformIgnorePatterns: [
   //   '\\\\node_modules\\\\',
   //   '\\.pnp\\.[^\\\\]+$'
