8ff05c46c2ad1406ab38fa77450cc9160bbbd387
dfkadyr <dfkadyr@gmail.com>
2022-12-08 19:37:33 +0300

    edit config for alias

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -18,6 +18,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
   }
   config.resolve!.modules = [paths.src, 'node_modules']
   config.resolve!.extensions?.push('.ts', '.tsx')
+  config.resolve!.alias = { '@': path.resolve(__dirname, '..', '..', 'src') }
 
   const rules = config.module!.rules as RuleSetRule[]
   config.module!.rules = rules.map((rule: RuleSetRule) => {
