615781ee75eb5a7f759eb6771308a1284d88b2be
dfkadyr <dfkadyr@gmail.com>
2022-12-21 21:39:04 +0300

    add storybook-addon-themes

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,10 +1,10 @@
 import { addDecorator } from '@storybook/react'
 
-import { Theme } from '../../src/app/providers/theme-provider'
 import { RouterDecorator } from '../../src/shared/config/storybook/router-decorator'
 import { StyleDecorator } from '../../src/shared/config/storybook/style-decorator'
 import { SuspenseDecorator } from '../../src/shared/config/storybook/suspense-decorator'
 import { ThemeDecorator } from '../../src/shared/config/storybook/theme-decorator'
+import { Theme } from '../../src/shared/const/theme'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -13,6 +13,15 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
+  },
+  layout: 'fullscreen',
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: Theme.LIGHT, color: '#f5f5f5' },
+      { name: 'dark', class: Theme.DARK, color: '#455a64' },
+      { name: 'orange', class: Theme.ORANGE, color: '#ffb74d' }
+    ]
   }
 }
 
